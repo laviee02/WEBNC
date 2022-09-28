fa0edd592a8a3c393ada07836da1d4e671e22201
VietTran <vn@741523.com>
2022-09-28 09:35:45 +0700

    thuchanh2

---
+++ TH2/admin/posts_connect.php
@@ -0,0 +1,4 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "1", "data");
+mysqli_set_charset($conn, 'UTF8');
+?>
---
+++ TH2/connect.php
@@ -0,0 +1,1 @@
+<?php $conn = mysqli_connect("localhost", "root", "1", "data"); mysqli_set_charset($conn, 'UTF8'); ?>
